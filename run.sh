efe6dade41c4f66be4cb0f5316d5301373ef07c8
user <vinaygoudmaduri@gmail.com>
2026-06-18 12:49:32 +0530

    training commit

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i $1.vdevops21.online, -e ansible_user=ec2-user -e ansible_password+ DevOps321 Roboshop.yml -e component_name=$1
+ansible-playbook -i $1.vdevops21.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 Roboshop.yml -e component_name=$1
